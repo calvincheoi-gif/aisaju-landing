6720b6ae321d4298d07995542997809889984125
calvincheoi-gif <calvincheoi@gmail.com>
2026-07-27 22:27:47 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import ServiceMenu from "@/components/ServiceMenu";
 import About from "@/components/About";
 import ConsultGuide from "@/components/ConsultGuide";
+import Reviews from "@/components/Reviews";
 import Channels from "@/components/Channels";
 import Footer from "@/components/Footer";
 
@@ -15,6 +16,7 @@ export default function Home() {
         <ServiceMenu />
         <About />
         <ConsultGuide />
+        <Reviews />
         <Channels />
       </main>
       <Footer />
