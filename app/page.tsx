a5d273d8aea5af2774fb7234f162a72d4e8f03a0
calvincheoi-gif <calvincheoi@gmail.com>
2026-08-15 15:24:50 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import HomeV6 from "@/components/HomeV6";
 
+// 홈은 항상 최신본으로 렌더 (CDN이 옛 페이지를 캐시하지 않도록)
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Home() {
   return <HomeV6 />;
 }
